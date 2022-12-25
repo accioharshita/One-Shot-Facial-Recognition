7877498b5dddf01ecbf8c2b2e738ca28a28c96a0
Harshita <66410468+accioharshita@users.noreply.github.com>
2022-12-25 11:57:37 +0530

    Upload File

---
+++ app/layers.py
@@ -0,0 +1,13 @@
+#importing dependencies
+
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+#Custom L1Dist layer(prerequisite for the model)
+
+class L1Dist(Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
